5f62aeeb9f178de7e3a72ceefd53b6d288699774
geronimo-lisboa <erikaeluciano2016@gmail.com>
2017-09-13 21:56:59 -0300

    Me baseando no exemplo 3 da nanogui. Falhei em fazer a combinação nanogui-vtk

--- main.cpp
+++ main.cpp
@@ -31,10 +31,41 @@ Color colval(0.5f, 0.5f, 0.7f, 1.f);
 
 int main(int argc, char ** argv)
 {
-	nanogui::init();
-	Screen * screen = new Screen(Vector2i(500, 700), "NanoGUI test");
+	glfwInit();
+	glfwSetTime(0);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, 0);
+	glfwWindowHint(GLFW_RED_BITS, 8);
+	glfwWindowHint(GLFW_GREEN_BITS, 8);
+	glfwWindowHint(GLFW_BLUE_BITS, 8);
+	glfwWindowHint(GLFW_ALPHA_BITS, 8);
+	glfwWindowHint(GLFW_STENCIL_BITS, 8);
+	glfwWindowHint(GLFW_DEPTH_BITS, 24);
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+	// Create a GLFWwindow object
+	GLFWwindow* window = glfwCreateWindow(800, 800, "example3", nullptr, nullptr);
+	if (window == nullptr) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glClearColor(0.2f, 0.25f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Create a nanogui screen and pass the glfw pointer to initialize
+	Screen *screen = new Screen();
+	screen->initialize(window, true);
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+	glfwSwapInterval(0);
+	glfwSwapBuffers(window);
+
 	FormHelper *gui = new FormHelper(screen);
-	ref<Window> window = gui->addWindow(Eigen::Vector2i(10, 10), "Form helper example");
+	ref<Window> nanoguiWindow = gui->addWindow(Eigen::Vector2i(10, 10), "Form helper example");
 	gui->addGroup("Basic types");
 	gui->addVariable("bool", bvar);
 	gui->addVariable("string", strval);
@@ -53,13 +84,14 @@ int main(int argc, char ** argv)
 
 	screen->setVisible(true);
 	screen->performLayout();
-	window->center();
+	nanoguiWindow->center();
 	//-----------NEW - COISAS DA VTK----------------
 	vtkSmartPointer<vtkRenderWindow> renderWindow;
 	vtkSmartPointer<vtkRenderer> renderer;
 	vtkSmartPointer<vtkRenderWindowInteractor> interactor;
 	renderer = vtkSmartPointer<vtkRenderer>::New();
 	renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->InitializeFromCurrentContext();
 	renderWindow->AddRenderer(renderer);
 	interactor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
 	interactor->SetRenderWindow(renderWindow);
@@ -80,7 +112,26 @@ int main(int argc, char ** argv)
 	renderWindow->Render();
 
 
-	nanogui::mainloop();//Esse m�todo blocka
+	// Game loop
+	while (!glfwWindowShouldClose(window)) {
+		// Check if any events have been activated (key pressed, mouse moved etc.) and call corresponding response functions
+		glfwPollEvents();
+
+		glClearColor(0.2f, 0.25f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		renderWindow->Render();
+		// Draw nanogui
+		screen->drawContents();
+		screen->drawWidgets();
+
+
+		glfwSwapBuffers(window);
+	}
+
+	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glfwTerminate();
+
+	return 0;
 
 	nanogui::shutdown();
 	return EXIT_SUCCESS;
